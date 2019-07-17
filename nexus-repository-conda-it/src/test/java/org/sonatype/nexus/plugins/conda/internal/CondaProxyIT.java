0fe1367a80d1170531bb59329f1d2f384802102a
Dan Rollo <danrollo@gmail.com>
2019-07-17 15:48:34 -0400

    fix new IT. Remove unused code.

--- nexus-repository-conda-it/src/test/java/org/sonatype/nexus/plugins/conda/internal/CondaProxyIT.java
+++ nexus-repository-conda-it/src/test/java/org/sonatype/nexus/plugins/conda/internal/CondaProxyIT.java
@@ -1,8 +1,5 @@
 package org.sonatype.nexus.plugins.conda.internal;
 
-import javax.cache.CacheManager;
-import javax.inject.Inject;
-
 import org.sonatype.goodies.httpfixture.server.fluent.Server;
 import org.sonatype.nexus.pax.exam.NexusPaxExamSupport;
 import org.sonatype.nexus.repository.Repository;
@@ -11,7 +8,6 @@ import org.sonatype.nexus.testsuite.testsupport.FormatClientSupport;
 import org.sonatype.nexus.testsuite.testsupport.NexusITSupport;
 
 import org.hamcrest.MatcherAssert;
-import org.junit.Before;
 import org.junit.Test;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
@@ -19,22 +15,18 @@ import org.ops4j.pax.exam.Option;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.sonatype.goodies.httpfixture.server.fluent.Behaviours.content;
+import static org.sonatype.goodies.httpfixture.server.fluent.Behaviours.error;
 import static org.sonatype.nexus.testsuite.testsupport.FormatClientSupport.status;
 
 public class CondaProxyIT
     extends CondaITSupport
 {
-  //private static final String TEST_PATH = "alphabet.txt";
   private static final String TEST_PATH = "imaginary/path/index.html";
   //private static final String TEST_PATH = "imaginary/path/osx/numpy-3.0.0-123.tar.bz2";
 
-  //private RawClient proxyClient;
   private CondaClient proxyClient;
 
   private Repository proxyRepo;
-
-  @Inject
-  private CacheManager cacheManager;
 
   @Configuration
   public static Option[] configureNexus() {
@@ -44,15 +36,19 @@ public class CondaProxyIT
     );
   }
 
-  @Before
-  public void setUpRepositories() throws Exception {
-    proxyRepo = repos.createCondaProxy("conda-test-proxy", "http://someCondaRemoteURL");
-    proxyClient = condaClient(proxyRepo);
-  }
-
   @Test
   public void unresponsiveRemoteProduces404() throws Exception {
-    MatcherAssert.assertThat(FormatClientSupport.status(proxyClient.get(TEST_PATH)), is(HttpStatus.NOT_FOUND));
+    Server server = Server.withPort(0).serve("/*")
+        .withBehaviours(error(HttpStatus.NOT_FOUND))
+        .start();
+    try {
+      proxyRepo = repos.createCondaProxy("conda-test-proxy-notfound", server.getUrl().toExternalForm());
+      proxyClient = condaClient(proxyRepo);
+      MatcherAssert.assertThat(FormatClientSupport.status(proxyClient.get(TEST_PATH)), is(HttpStatus.NOT_FOUND));
+    }
+    finally {
+      server.stop();
+    }
   }
 
   @Test
@@ -61,7 +57,8 @@ public class CondaProxyIT
         .withBehaviours(content("Response"))
         .start();
     try {
-      proxyClient = condaClient(repos.createCondaProxy("conda-test-proxy-offline", server.getUrl().toExternalForm()));
+      proxyRepo = repos.createCondaProxy("conda-test-proxy-offline", server.getUrl().toExternalForm());
+      proxyClient = condaClient(proxyRepo);
       proxyClient.get(TEST_PATH);
     }
     finally {
